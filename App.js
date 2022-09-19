bdf54eff4fee35c576046dfa8247fbc80b388f90
np666339 <nopost22@gmail.com>
2022-09-18 21:42:33 -0500

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  Image,
+  Text,
+  FlatList,
+} from 'react-native';
+import data from './location.json';
+import * as SplashScreen from 'expo-splash-screen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+SplashScreen.preventAutoHideAsync();
+setTimeout(SplashScreen.hideAsync, 2000);
+
+const chicagoIcon = require('./assets/chicago.png');
+
+export default class App extends React.Component { 
+
+  renderRow({item}) {
+    return (
+      <View style={styles.row}>
+        <View style={styles.iconContainer}>
+          <Image source={chicagoIcon} style={styles.icon} />
+        </View>
+        <View style={styles.info}>
+          <Text style={styles.place}>{item.place}</Text>
+          <Text style={styles.address}>{item.address}</Text>
+          <Text style={styles.url}>{item.modurl}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <Text style={styles.title}>City guide</Text>
+        <FlatList data={data} renderItem={this.renderRow} />
+      </View>
+    );
+  }
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ede2e0',
+  },
+  title: {
+    backgroundColor: '#1c7da5',
+    color: '#ede2e0',
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 10,
+    paddingTop: 40,
+    textAlign: 'center',
+  },
+  row: {
+    borderColor: '#7b7983',
+    borderBottomWidth: 1,
+    flexDirection: 'row',
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  iconContainer: {
     alignItems: 'center',
+    backgroundColor: '#1f3243',
+    borderColor: '#1f3243',
+    borderRadius: 40,
+    borderWidth: 1,
     justifyContent: 'center',
+    height: 70,
+    width: 70,
+  },
+  icon: {
+    height: 45,
+    width: 45,
+  },
+  info: {
+    flex: 1,
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  place: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  address: {
+    color: '#3c4f62',
+    fontSize: 16,
+  },
+  url: {
+    color: '#3c4f62',
+    fontSize: 16,
+    fontStyle: 'italic'
   },
 });
